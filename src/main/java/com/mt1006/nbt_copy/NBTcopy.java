6119f2d61e4039a6b5b0be7110e1face13130445
mt1006 <damianm916@gmail.com>
2024-02-28 17:13:13 +0100

    Version 1.0

---
+++ src/main/java/com/mt1006/nbt_copy/NBTcopy.java
@@ -0,0 +1,34 @@
+package com.mt1006.nbt_copy;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.network.chat.*;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(NBTcopy.MOD_ID)
+public class NBTcopy
+{
+	public static final String MOD_ID = "nbt_copy";
+	public static final String VERSION = "1.0";
+	public static final String FOR_VERSION = "1.20.1";
+	public static final String FOR_LOADER = "Forge";
+	public static final Logger LOGGER = LogUtils.getLogger();
+
+	public NBTcopy()
+	{
+		LOGGER.info(getFullName() + " - Author: mt1006 (mt1006x)");
+	}
+
+	public static String getFullName()
+	{
+		return "NBTcopy v" + VERSION + " for Minecraft " + FOR_VERSION + " [" + FOR_LOADER + "]";
+	}
+
+	public static MutableComponent withCopyButton(Component component, String textToCopy)
+	{
+		ClickEvent clickEvent = new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, textToCopy);
+		HoverEvent hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.translatable("nbt_copy.copy_button_info"));
+		return component.copy().append(Component.translatable("nbt_copy.copy_button")
+				.setStyle(Style.EMPTY.withClickEvent(clickEvent).withHoverEvent(hoverEvent)));
+	}
+}
