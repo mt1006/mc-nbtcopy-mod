a14b58ab20351a388ffacac5a2bb278b055b03a9
mt1006 <damianm916@gmail.com>
2024-02-28 17:11:17 +0100

    Version 1.0

---
+++ src/main/java/com/mt1006/nbt_copy/NBTcopy.java
@@ -0,0 +1,32 @@
+package com.mt1006.nbt_copy;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.network.chat.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class NBTcopy implements ModInitializer
+{
+	public static final String VERSION = "1.0";
+	public static final String FOR_VERSION = "1.16.5";
+	public static final String FOR_LOADER = "Fabric";
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override public void onInitialize()
+	{
+		LOGGER.info(getFullName() + " - Author: mt1006 (mt1006x)");
+	}
+
+	public static String getFullName()
+	{
+		return "NBTcopy v" + VERSION + " for Minecraft " + FOR_VERSION + " [" + FOR_LOADER + "]";
+	}
+
+	public static MutableComponent withCopyButton(Component component, String textToCopy)
+	{
+		ClickEvent clickEvent = new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, textToCopy);
+		HoverEvent hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TranslatableComponent("nbt_copy.copy_button_info"));
+		return component.copy().append(new TranslatableComponent("nbt_copy.copy_button")
+				.setStyle(Style.EMPTY.withClickEvent(clickEvent).withHoverEvent(hoverEvent)));
+	}
+}
