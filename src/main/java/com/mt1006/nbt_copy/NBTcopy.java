74209330492b26634b1c08515d5cf5df16aaed17
mt1006 <damianm916@gmail.com>
2024-07-25 16:10:08 +0200

    Version 1.0.2

--- src/main/java/com/mt1006/nbt_copy/NBTcopy.java
+++ src/main/java/com/mt1006/nbt_copy/NBTcopy.java
@@ -1,28 +1,12 @@
 package com.mt1006.nbt_copy;
 
-import com.mojang.logging.LogUtils;
 import net.minecraft.network.chat.*;
 import net.minecraftforge.fml.common.Mod;
-import org.slf4j.Logger;
 
-@Mod(NBTcopy.MOD_ID)
+@Mod("nbt_copy")
 public class NBTcopy
 {
-	public static final String MOD_ID = "nbt_copy";
-	public static final String VERSION = "1.0";
-	public static final String FOR_VERSION = "1.20.1";
-	public static final String FOR_LOADER = "Forge";
-	public static final Logger LOGGER = LogUtils.getLogger();
-
-	public NBTcopy()
-	{
-		LOGGER.info(getFullName() + " - Author: mt1006 (mt1006x)");
-	}
-
-	public static String getFullName()
-	{
-		return "NBTcopy v" + VERSION + " for Minecraft " + FOR_VERSION + " [" + FOR_LOADER + "]";
-	}
+	public static boolean skipButton = false;
 
 	public static MutableComponent withCopyButton(Component component, String textToCopy)
 	{
