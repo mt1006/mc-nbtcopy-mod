19b779fe790364d4280747fa6ec1d8d0ea3396c4
mt1006 <mt1006@proton.me>
2025-03-26 20:36:42 +0100

    Version 1.0.4

--- src/main/java/net/mt1006/nbtcopy/NBTcopy.java
+++ src/main/java/net/mt1006/nbtcopy/NBTcopy.java
@@ -1,9 +1,9 @@
-package com.mt1006.nbt_copy;
+package net.mt1006.nbtcopy;
 
 import net.minecraft.network.chat.*;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod("nbt_copy")
+@Mod("nbtcopy")
 public class NBTcopy
 {
 	public static boolean skipButton = false;
@@ -11,8 +11,8 @@ public class NBTcopy
 	public static MutableComponent withCopyButton(Component component, String textToCopy)
 	{
 		ClickEvent clickEvent = new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, textToCopy);
-		HoverEvent hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.translatable("nbt_copy.copy_button_info"));
-		return component.copy().append(Component.translatable("nbt_copy.copy_button")
+		HoverEvent hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.translatable("nbtcopy.copy_button_info"));
+		return component.copy().append(Component.translatable("nbtcopy.copy_button")
 				.setStyle(Style.EMPTY.withClickEvent(clickEvent).withHoverEvent(hoverEvent)));
 	}
 }
